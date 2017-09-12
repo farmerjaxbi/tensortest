e061e095f67218f780278ab75ca802902ed83fbc
farmerjaxbi <james.baxter@woodside.com.au>
2017-09-12 17:08:21 +0800

    second

--- tensortest.py
+++ tensortest.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from sklearn import datasets
 from tensorflow.python.framework import ops
 ops.reset_default_graph()
-sess = tf.Session
+sess = tf.Session()
 iris = datasets.load_iris()
 
 x_vals = np.array([x[3] for x in iris.data])
@@ -20,8 +20,43 @@ b = tf.Variable(tf.random_normal(shape=[1,1]))
 model_output = tf.add(tf.matmul(x_data, A), b)
 
 loss = tf.reduce_mean(tf.square(y_target - model_output))
-init = tf.global_variables_initializer()
-sess.run(init)
+# init = tf.global_variables_initializer()
+sess.run(tf.global_variables_initializer())
 my_opt = tf.train.GradientDescentOptimizer(learning_rate)
 train_step = my_opt.minimize(loss)
 
+loss_vec = []
+for i in range(100):
+    rand_index = np.random.choice(len(x_vals), size=batch_size)
+    rand_x = np.transpose([x_vals[rand_index]])
+    rand_y = np.transpose([y_vals[rand_index]])
+    sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+    temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+    loss_vec.append(temp_loss)
+    if (i+1)%25==0:
+        print('Step #' + str(i+1) + ' A = ' + str(sess.run(A)) + 'b = ' + str(sess.run(b)))
+        print('Loss = ''' + str(temp_loss))
+# Step #25 A = [[ 2.17270374]] b = [[ 2.85338426]]
+# Loss = 1.08116
+# Step #50 A = [[ 1.70683455]] b = [[ 3.59916329]]
+# Loss = 0.796941
+# Step #75 A = [[ 1.32762754]] b = [[ 4.08189011]]
+# Loss = 0.466912
+# Step #100 A = [[ 1.15968263]] b = [[ 4.38497639]]
+# Loss = 0.281003
+
+
+[slope] = sess.run(A)
+[y_intercept] = sess.run(b)
+best_fit = []
+for i in x_vals:
+    best_fit.append(slope*i+y_intercept)
+
+plt.plot(x_vals, y_vals, 'o', label='Data Points')
+plt.plot(x_vals, best_fit, 'r-', label='Best fit line', linewidth=3)
+plt.legend(loc='upper left')
+plt.title('Sepal Length vs Pedal Width')
+plt.show()
+plt.plot(loss_vec, 'k-')
+plt.title('L2 Loss per Generation')
+plt.show()
