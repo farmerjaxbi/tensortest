69018d0c54440702235e613fa80f159256a934ee
farmerjax <farmerjax@gmail.com>
2017-09-12 13:29:41 +0800

    second

--- tensortest.py
+++ tensortest.py
@@ -1,1 +1,27 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from sklearn import datasets
+from tensorflow.python.framework import ops
+ops.reset_default_graph()
+sess = tf.Session
+iris = datasets.load_iris()
+
+x_vals = np.array([x[3] for x in iris.data])
+y_vals = np.array([y[0] for y in iris.data])
+
+learning_rate = 0.05
+batch_size = 25
+x_data = tf.placeholder(shape=[None,1], dtype=tf.float32)
+y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+A = tf.Variable(tf.random_normal(shape=[1,1]))
+b = tf.Variable(tf.random_normal(shape=[1,1]))
+
+model_output = tf.add(tf.matmul(x_data, A), b)
+
+loss = tf.reduce_mean(tf.square(y_target - model_output))
+init = tf.global_variables_initializer()
+sess.run(init)
+my_opt = tf.train.GradientDescentOptimizer(learning_rate)
+train_step = my_opt.minimize(loss)
+
